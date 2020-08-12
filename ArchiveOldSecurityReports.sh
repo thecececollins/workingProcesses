f21d3c143a5ee3edd336a48db3a92718cdcae76f
Cece Collins <7505271+thecececollins@users.noreply.github.com>
2020-08-12 10:08:23 -0500

    Update ArchiveOldSecurityReports.sh

--- ArchiveOldSecurityReports.sh
+++ ArchiveOldSecurityReports.sh
@@ -4,9 +4,8 @@
 # This script will accomplish the following:
 #  			- Every month will move the old audit report to archive folder in AWS
 #
-# Written by: Collins | IT Lead | Narrative Science
-# Edited by: Collins | IT Lead | Narrative Science
-#
+# Written by: Collins
+# Edited by: Collins
 # Created On: March 3, 2020
 # Updated On: March 3, 2020
 # 
@@ -20,8 +19,8 @@ thisYear=$(date +'%Y')
 
 #Move last month's Google Apps report to archive folder in AWS
 echo "Moving last month's report to archive folder in AWS"
-aws2 s3 mv s3://s3-ns-devops-secure/monthly_security_audit/Google-UserReport-$thisYear$thisMonth.csv  s3://s3-ns-devops-secure/archived_monthly_security_audit/Google-UserReport-$thisYear$thisMonth.csv
+aws2 s3 mv s3://s3-ns-[folder name]/monthly_security_audit/Google-UserReport-$thisYear$thisMonth.csv  s3://s3-[folder name]/archived_monthly_security_audit/Google-UserReport-$thisYear$thisMonth.csv
 
-aws2 s3 mv s3://s3-ns-devops-secure/monthly_security_audit/Github-UserReport-$thisYear$thisMonth.csv  s3://s3-ns-devops-secure/archived_monthly_security_audit/Github-UserReport-$thisYear$thisMonth.csv
+aws2 s3 mv s3://s3-[folder name]/monthly_security_audit/Github-UserReport-$thisYear$thisMonth.csv  s3://s3-[folder name]/archived_monthly_security_audit/Github-UserReport-$thisYear$thisMonth.csv
 
-aws2 s3 mv s3://s3-ns-devops-secure/monthly_security_audit/AWS-AllUserReport-$thisYear$thisMonth.csv  s3://s3-ns-devops-secure/archived_monthly_security_audit/AWS-AllUserReport-$thisYear$thisMonth.csv
+aws2 s3 mv s3://s3-[folder name]/monthly_security_audit/AWS-AllUserReport-$thisYear$thisMonth.csv  s3://s3-[folder name]/archived_monthly_security_audit/AWS-AllUserReport-$thisYear$thisMonth.csv
